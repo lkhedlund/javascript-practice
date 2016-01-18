437155aab58fee4a2716e86c3eac381597843a77
Lars Hedlund <lkhedlund@gmail.com>
2016-01-18 15:25:24 -0800

    Added linear scale

--- D3tutorials/barChart/main.js
+++ D3tutorials/barChart/main.js
@@ -2,11 +2,15 @@
 // http://bost.ocks.org/mike/bar/
 
 var data = [4, 8, 15, 16, 23, 42];
-
 var chart = d3.select(".chart");
 var bar = chart.selectAll("div");
 var barUpdate = bar.data(data);
 var barEnter =  barUpdate.enter().append("div");
 
-barEnter.style("width", function(d) { return d * 10 + "px"; });
+// Linear scale for a more explicit chart display
+var x = d3.scale.linear()
+  .domain([0, d3.max(data)])
+  .range([0, 420]);
+
+barEnter.style("width", function(d) { return x(d) + "px"; });
 barEnter.text(function(d) { return d; });
