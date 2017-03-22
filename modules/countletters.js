e5e7769f6703d7866437bedb81ae328afd61aca5
Lars Hedlund <lkhedlund@gmail.com>
2017-03-22 14:00:05 -0700

    Added letter counting practice script

--- modules/countletters.js
+++ modules/countletters.js
@@ -1,3 +1,24 @@
-(function() {
+const countLetters = (function(sentence) {
 
+  function countIn(sentence) {
+    let totalCount = {};
+    sentence = sentence.replace(/\s/g, '');
+
+    for (index in sentence) {
+      let letter = sentence[index];
+      if (totalCount[letter] !== undefined) {
+        totalCount[letter] += 1;
+      } else {
+        totalCount[letter] = 1;
+      }
+    }
+
+    return totalCount;
+  }
+
+  return {
+    countIn: countIn,
+  }
 })();
+
+console.log( countLetters.countIn(process.argv[2]) );
