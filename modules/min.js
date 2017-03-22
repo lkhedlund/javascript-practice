bbd19a95947a52206ab211e5821df3f62af85818
Lars Hedlund <lkhedlund@gmail.com>
2017-03-22 13:44:13 -0700

    Converted to use Revealing pattern

--- modules/min.js
+++ modules/min.js
@@ -1,12 +1,19 @@
-function min(numbers) {
-  var minimum = numbers[0];
-  numbers.forEach(function(number) {
-    if (number < minimum) {
-      minimum = number;
-    }
-  });
-  return minimum;
-}
+const myMath = (function(numbers) {
+  function min(numbers) {
+    var minimum = numbers[0];
+    numbers.forEach(function(number) {
+      if (number < minimum) {
+        minimum = number;
+      }
+    });
+    return minimum;
+  }
+
+  return {
+    min: min,
+  }
+
+})();
 
 
 /*
@@ -15,13 +22,13 @@ function min(numbers) {
  */
 
 var flightPrices = [1260, 490, 599, 1400, 820];
-console.log("The cheapest flight amongst $1260, $490, $599, $1400 and $820 costs $" + min(flightPrices));
+console.log("The cheapest flight amongst $1260, $490, $599, $1400 and $820 costs $" + myMath.min(flightPrices));
 
 var golfScores = [-1, 3, 0, -4, 1, 4, -2];
-console.log("The winning golf score amongst -1, 3, 0, -4, 1, 4 and 1 is " + min(golfScores));
+console.log("The winning golf score amongst -1, 3, 0, -4, 1, 4 and 1 is " + myMath.min(golfScores));
 
 var pageNumbers = [232];
-console.log("The shortest book out of a list of single book with 232 pages has " + min(pageNumbers) + " pages");
+console.log("The shortest book out of a list of single book with 232 pages has " + myMath.min(pageNumbers) + " pages");
 
 var temperatures = [45, 10, -20, 0, 3, -20];
-console.log("The coldest temperature amongst 45C, 10C, -20C, 0C, 3C and -20C is " + min(temperatures) + "C");
+console.log("The coldest temperature amongst 45C, 10C, -20C, 0C, 3C and -20C is " + myMath.min(temperatures) + "C");
--- modules/password.js
+++ modules/password.js
@@ -1,27 +1,31 @@
-const password = process.argv[2];
+const passwordObfuscator = (function(password) {
+  function obfuscate(letter) {
+    const rules = {
+      'a': '4',
+      'e': '3',
+      'o': '0',
+      'l': '1',
+    }
 
-function obfuscate(letter) {
-  const rules = {
-    'a': '4',
-    'e': '3',
-    'o': '0',
-    'l': '1',
+    if (rules[letter] === undefined) {
+      return letter;
+    } else {
+      return rules[letter];
+    }
   }
 
-  if (rules[letter] === undefined) {
-    return letter;
-  } else {
-    return rules[letter];
+  function convertPassword(password) {
+    let newPassword = '';
+
+    for (i = 0; i < password.length; i++) {
+      newPassword += obfuscate(password[i]);
+    }
+    return newPassword;
   }
-}
 
-function createPassword(password) {
-  let newPassword = '';
-
-  for (i = 0; i < password.length; i++) {
-    newPassword += obfuscate(password[i]);
+  return {
+    convert: convertPassword,
   }
-  return newPassword;
-}
+})();
 
-console.log( createPassword(password) );
+console.log(passwordObfuscator.convert(process.argv[2]));
