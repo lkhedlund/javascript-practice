d8db4644e11452d1295ee946969b6e6a7739c5f5
Lars Hedlund <lkhedlund@gmail.com>
2017-04-01 08:53:39 -0700

    Bash on Windows changes

---
+++ react/simple-chat/src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Chatbar from './Chatbar.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <Chatbar />
+    );
+  }
+}
+export default App;
---
+++ react/simple-chat/src/Chatbar.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Chatbar extends Component {
+  render () {
+    return (
+      <h1>This is a chatbar</h1>
+    );
+  }
+}
+
+export default Chatbar;
