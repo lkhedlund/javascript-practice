da08d86b1184e5a0a9ad37a9e3c67f459429498c
Lars Hedlund <lkhedlund@gmail.com>
2017-04-08 14:06:47 -0700

    Added redux store

---
+++ react/voting-server/test/store_spec.js
@@ -0,0 +1,19 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import makeStore from '../src/store';
+
+describe('store', () => {
+  it('is a Redux store configured with the correct reducer', () => {
+    const store = makeStore();
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'SET_ENTRIES',
+      entries: ['Moonrise Kingdom', 'The Grand Budapest Hotel']
+    });
+    expect(store.getState()).to.equal(fromJS({
+      entries: ['Moonrise Kingdom', 'The Grand Budapest Hotel']
+    }));
+  });
+});
